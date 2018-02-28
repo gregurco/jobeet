cf578a2e655fcd2032d51531f9a32eed8b7a471a
Gregurco Vlad <gregurco.vlad@gmail.com>
2018-02-28 20:46:05 +0200

    Create list and show actions for jobs

---
+++ src/Controller/JobController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Job;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @Route("job")
+ */
+class JobController extends AbstractController
+{
+    /**
+     * Lists all job entities.
+     *
+     * @Route("/", name="job.list")
+     *
+     * @return Response
+     */
+    public function listAction() : Response
+    {
+        $jobs = $this->getDoctrine()->getRepository(Job::class)->findAll();
+
+        return $this->render('job/list.html.twig', [
+            'jobs' => $jobs,
+        ]);
+    }
+
+    /**
+     * Finds and displays a job entity.
+     *
+     * @Route("/{id}", name="job.show")
+     *
+     * @param Job $job
+     *
+     * @return Response
+     */
+    public function showAction(Job $job) : Response
+    {
+        return $this->render('job/show.html.twig', [
+            'job' => $job,
+        ]);
+    }
+}
