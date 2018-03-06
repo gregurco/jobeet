e2988f4a7545599c4097dd96e317d063a24e9c43
Gregurco Vlad <gregurco.vlad@gmail.com>
2018-03-06 22:24:41 +0200

    Install stof/doctrine-extensions-bundle package Setup slug for category entity

---
+++ src/Migrations/Version20180306193355.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types = 1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180306193355 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE categories ADD slug VARCHAR(128) NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_3AF34668989D9B62 ON categories (slug)');
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP INDEX UNIQ_3AF34668989D9B62 ON categories');
+        $this->addSql('ALTER TABLE categories DROP slug');
+    }
+}
