452de3115bdc3f2f8a7e46d11088c61662acb172
Gregurco Vlad <gregurco.vlad@gmail.com>
2018-03-09 13:10:18 +0200

    Install knp-paginator-bundle Implement pagination on category page
    (cherry picked from commit 99cb366)

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
