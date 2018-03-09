99cb36688c3c813699fe3b25cc6935bb5a81b9d9
Gregurco Vlad <gregurco.vlad@gmail.com>
2018-03-09 13:10:18 +0200

    Install knp-paginator-bundle Implement pagination on category page

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -3,27 +3,42 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use App\Entity\Job;
+use Knp\Component\Pager\PaginatorInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 
-class CategoryController extends AbstractController
+class CategoryController extends Controller
 {
     /**
      * Finds and displays a category entity.
      *
-     * @Route("/category/{slug}", name="category.show")
+     * @Route("/category/{slug}/{page}", name="category.show", defaults={"page": 1})
      * @Method("GET")
      *
      * @param Category $category
+     * @param PaginatorInterface $paginator
+     * @param int $page
      *
      * @return Response
      */
-    public function showAction(Category $category) : Response
+    public function showAction(
+        Category $category,
+        int $page,
+        PaginatorInterface $paginator
+    ) : Response
     {
+        $activeJobs = $paginator->paginate(
+            $this->getDoctrine()->getRepository(Job::class)->getPaginatedActiveJobsByCategoryQuery($category),
+            $page,
+            $this->getParameter('max_jobs_on_category')
+        );
+
         return $this->render('category/show.html.twig', array(
             'category' => $category,
+            'activeJobs' => $activeJobs,
         ));
     }
 }
