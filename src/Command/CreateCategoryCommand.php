69c95e41e1deab16057c192d322f2d04a913f3db
Gregurco Vlad <gregurco.vlad@gmail.com>
2018-04-03 00:41:18 +0300

    Interacting with user in console command

--- src/Command/CreateCategoryCommand.php
+++ src/Command/CreateCategoryCommand.php
@@ -7,6 +7,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
 
 class CreateCategoryCommand extends Command
 {
@@ -38,6 +39,27 @@ class CreateCategoryCommand extends Command
 
             // configure an argument
             ->addArgument('name', InputArgument::REQUIRED, 'The name of the category.');
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     */
+    protected function interact(InputInterface $input, OutputInterface $output)
+    {
+        if (!$input->getArgument('name')) {
+            $question = new Question('Please choose a name: ');
+            $question->setValidator(function ($name) {
+                if (empty($name)) {
+                    throw new \Exception('Name can not be empty');
+                }
+
+                return $name;
+            });
+
+            $answer = $this->getHelper('question')->ask($input, $output, $question);
+            $input->setArgument('name', $answer);
+        }
     }
 
     /**
