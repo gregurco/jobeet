0373b61358348598dfae0c21b58ef3211640c224
Gregurco Vlad <gregurco.vlad@gmail.com>
2018-04-03 00:14:25 +0300

    Pass name to create category command

--- src/Command/CreateCategoryCommand.php
+++ src/Command/CreateCategoryCommand.php
@@ -3,6 +3,7 @@
 namespace App\Command;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -19,7 +20,10 @@ class CreateCategoryCommand extends Command
 
             // the full command description shown when running the command with
             // the "--help" option
-            ->setHelp('This command allows you to add new category in db...');
+            ->setHelp('This command allows you to add new category in db...')
+
+            // configure an argument
+            ->addArgument('name', InputArgument::REQUIRED, 'The name of the category.');
     }
 
     /**
@@ -28,6 +32,14 @@ class CreateCategoryCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        // ...
+        // outputs multiple lines to the console (adding "\n" at the end of each line)
+        $output->writeln([
+            'Category Creator',
+            '============',
+            '',
+        ]);
+
+        // retrieve the argument value using getArgument()
+        $output->writeln('Name: '.$input->getArgument('name'));
     }
 }
