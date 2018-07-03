8b0248008cbc322bb7f6c932c90f8021ef572a23
Vlad Gregurco <gregurco.vlad@gmail.com>
2018-07-03 09:31:08 +0300

    Day 10: Admin

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -40,7 +40,7 @@ class Category
     /**
      * @var Job[]|ArrayCollection
      *
-     * @ORM\OneToMany(targetEntity="Job", mappedBy="category")
+     * @ORM\OneToMany(targetEntity="Job", mappedBy="category", cascade={"remove"})
      */
     private $jobs;
 
