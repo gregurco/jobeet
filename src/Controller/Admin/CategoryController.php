551c46c9ee163dae41ce8ee89f31355fb465f2d2
Vlad Gregurco <gregurco.vlad@gmail.com>
2018-07-03 09:31:08 +0300

    Day 10: Admin

---
+++ src/Controller/Admin/CategoryController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Category;
+use App\Form\Admin\CategoryType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class CategoryController extends AbstractController
+{
+    /**
+     * Lists all categories entities.
+     *
+     * @Route("/admin/categories", name="admin.category.list", methods="GET")
+     *
+     * @param EntityManagerInterface $em
+     *
+     * @return Response
+     */
+    public function list(EntityManagerInterface $em) : Response
+    {
+        $categories = $em->getRepository(Category::class)->findAll();
+
+        return $this->render('admin/category/list.html.twig', [
+            'categories' => $categories,
+        ]);
+    }
+
+    /**
+     * Create category.
+     *
+     * @Route("/admin/category/create", name="admin.category.create", methods="GET|POST")
+     *
+     * @param Request $request
+     * @param EntityManagerInterface $em
+     *
+     * @return Response
+     */
+    public function create(Request $request, EntityManagerInterface $em) : Response
+    {
+        $category = new Category();
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($category);
+            $em->flush();
+
+            return $this->redirectToRoute('admin.category.list');
+        }
+
+        return $this->render('admin/category/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * Create category.
+     *
+     * @Route("/admin/category/{id}/edit", name="admin.category.edit", methods="GET|POST")
+     *
+     * @param Request $request
+     * @param EntityManagerInterface $em
+     * @param Category $category
+     *
+     * @return Response
+     */
+    public function edit(Request $request, EntityManagerInterface $em, Category $category) : Response
+    {
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+
+            return $this->redirectToRoute('admin.category.list');
+        }
+
+        return $this->render('admin/category/edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * Delete category.
+     *
+     * @Route("/admin/category/{id}/delete", name="admin.category.delete", methods="DELETE")
+     *
+     * @param Request $request
+     * @param EntityManagerInterface $em
+     * @param Category $category
+     *
+     * @return Response
+     */
+    public function delete(Request $request, EntityManagerInterface $em, Category $category) : Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $category->getId(), $request->request->get('_token'))) {
+            $em->remove($category);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('admin.category.list');
+    }
+}
---
+++ src/Form/Admin/CategoryType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form\Admin;
+
+use App\Entity\Category;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class CategoryType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name', TextType::class, [
+            'constraints' => [
+                new NotBlank(),
+                new Length(['max' => 100]),
+            ]
+        ]);
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Category::class,
+        ]);
+    }
+}
