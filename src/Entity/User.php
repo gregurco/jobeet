0e2ab5c8cf00ffba0055ae89182decfe303c6a6e
Vlad Gregurco <gregurco.vlad@gmail.com>
2018-07-29 15:44:46 +0300

    Day 11: install and configure friendsofsymfony/user-bundle

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -19,9 +19,4 @@ class User extends BaseUser
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
-
-    public function __construct()
-    {
-        parent::__construct();
-    }
 }
