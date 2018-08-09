7eae1caa0474552fb92198f4a4d0056c05917f9a
Vitalina Hrapeiciuc <vitalina.hrapeichuk@gmail.com>
2018-08-09 14:24:26 +0300

    Create affiliate form

---
+++ src/Controller/AffiliateController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Affiliate;
+use App\Form\AffiliateType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class AffiliateController extends AbstractController
+{
+    /**
+     * Creates a new affiliate entity.
+     *
+     * @Route("/affiliate/create", name="affiliate.create", methods={"GET", "POST"})
+     *
+     * @param Request $request
+     * @param EntityManagerInterface $em
+     *
+     * @return RedirectResponse|Response
+     */
+    public function create(Request $request, EntityManagerInterface $em) : Response
+    {
+        $affiliate = new Affiliate();
+        $form = $this->createForm(AffiliateType::class, $affiliate);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $affiliate->setActive(false);
+
+            $em->persist($affiliate);
+            $em->flush();
+        }
+
+        return $this->render('affiliate/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
