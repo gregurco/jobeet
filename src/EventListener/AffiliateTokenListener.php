2043dd8ad648c76755cfbffbd7d77c54a0f1d77a
Vitalina Hrapeiciuc <vitalina.hrapeichuk@gmail.com>
2018-08-09 14:24:26 +0300

    Create affiliate form

---
+++ src/EventListener/AffiliateTokenListener.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\EventListener;
+
+use App\Entity\Affiliate;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+
+class AffiliateTokenListener
+{
+    /**
+     * @param LifecycleEventArgs $args
+     */
+    public function prePersist(LifecycleEventArgs $args)
+    {
+        $entity = $args->getEntity();
+
+        if (!$entity instanceof Affiliate) {
+            return;
+        }
+
+        if (!$entity->getToken()) {
+            $entity->setToken(\bin2hex(\random_bytes(10)));
+        }
+    }
+}
