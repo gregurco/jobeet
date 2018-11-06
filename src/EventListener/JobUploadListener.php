8ad85c3cb38a7e393a5c7cc9bc703b4f27ca3c09
Vlad Gregurco <gregurco.vlad@gmail.com>
2018-11-07 01:42:51 +0200

    Rebase conflict

--- src/EventListener/JobUploadListener.php
+++ src/EventListener/JobUploadListener.php
@@ -54,22 +54,6 @@ class JobUploadListener
     }
 
     /**
-     * @param LifecycleEventArgs $args
-     */
-    public function postLoad(LifecycleEventArgs $args)
-    {
-        $entity = $args->getEntity();
-
-        if (!$entity instanceof Job) {
-            return;
-        }
-
-        if ($fileName = $entity->getLogo()) {
-            $entity->setLogo(new File($this->uploader->getTargetDirectory() . '/' . $fileName));
-        }
-    }
-
-    /**
      * @param $entity
      */
     private function uploadFile($entity)
