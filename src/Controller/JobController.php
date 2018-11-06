9248a9ff50e064be8b146c6a1099d524566110ac
Vlad Gregurco <gregurco.vlad@gmail.com>
2018-11-07 01:24:38 +0200

    Rebase conflict

--- src/Controller/JobController.php
+++ src/Controller/JobController.php
@@ -5,12 +5,10 @@ namespace App\Controller;
 use App\Entity\Category;
 use App\Entity\Job;
 use App\Form\JobType;
-use App\Service\FileUploader;
 use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\FormInterface;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -61,26 +59,16 @@ class JobController extends AbstractController
      *
      * @param Request $request
      * @param EntityManagerInterface $em
-     * @param FileUploader $fileUploader
      *
      * @return RedirectResponse|Response
      */
-    public function create(Request $request, EntityManagerInterface $em, FileUploader $fileUploader) : Response
+    public function create(Request $request, EntityManagerInterface $em) : Response
     {
         $job = new Job();
         $form = $this->createForm(JobType::class, $job);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            /** @var UploadedFile|null $logoFile */
-            $logoFile = $form->get('logo')->getData();
-
-            if ($logoFile instanceof UploadedFile) {
-                $fileName = $fileUploader->upload($logoFile);
-
-                $job->setLogo($fileName);
-            }
-
             $em->persist($job);
             $em->flush();
 
