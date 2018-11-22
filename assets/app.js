ec3936924c6e9cb87bed49e325d23e52dd573a43
Vitalina Hrapeiciuc <vitalina.hrapeichuk@gmail.com>
2018-11-22 21:28:27 +0200

    Install jQuery

---
+++ assets/app.js
@@ -0,0 +1,11 @@
+// require jQuery normally
+var $ = require('jquery');
+
+// create global $ and jQuery variables
+global.$ = global.jQuery = $;
+
+require('bootstrap');
+
+$(document).ready(function() {
+  $(".dropdown-toggle").dropdown();
+});
