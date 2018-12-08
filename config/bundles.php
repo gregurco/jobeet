266a5f2c5de6638da5257a0029c99ee1a19e8f1a
Vlad Gregurco <gregurco.vlad@gmail.com>
2018-12-08 15:01:33 +0200

    Connect Sentry service

--- config/bundles.php
+++ config/bundles.php
@@ -20,4 +20,5 @@ return [
     FOS\UserBundle\FOSUserBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+    Sentry\SentryBundle\SentryBundle::class => ['all' => true],
 ];
