4e1ac000cca75ed432abd3e114bccc184f130b26
Vlad Gregurco <gregurco.vlad@gmail.com>
2019-03-07 01:27:28 +0200

    Upgrade to Symfony 4.2

--- config/bundles.php
+++ config/bundles.php
@@ -20,4 +20,5 @@ return [
     FOS\UserBundle\FOSUserBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
